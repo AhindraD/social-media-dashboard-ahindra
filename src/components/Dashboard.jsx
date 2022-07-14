019e2bb4a786c94cb5da50e3729dc5ddd4233ebf
Ahindra D <official.ahindra@gmail.com>
2022-07-14 14:19:40 +0530

    layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,33 @@
+import BigCards from "./BigCards";
+import SmallCards from "./SmallCards";
 
 
-function Dashboard(props){
+function Dashboard(props) {
 
 
-    return(
+    return (
         <div className="container">
-
+            <section className="dash">
+                <div className="head1">
+                    <div className="h1">
+                        <p className="h1top"></p>
+                        <p className="h1bottom"></p>
+                    </div>
+                    <div className="toggle">
+                    </div>
+                </div>
+                <main className="big">
+                    <BigCards />
+                </main>
+            </section>
+            <section className="overview">
+                <div className="head2">
+                    <p className="h2"></p>
+                </div>
+                <main className="small">
+                    <SmallCards/>
+                </main>
+            </section>
         </div>
     )
 }
